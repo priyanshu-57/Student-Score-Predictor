c175ee2d0cd4e86241ac19ba7ead2b1011a1ba97
Priyanshu Kushawaha <kushawahapriyanshu57@gmail.com>
2026-05-17 21:08:38 +0545

    tested

--- test.py
+++ test.py
@@ -2,5 +2,24 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-df= pd.read_csv("student_data.csv")
-print(df)
+data= pd.read_csv("student_data.csv")
+
+#features input
+X= data[["hours_studied", "sleep_hours", "attendance"]]
+y= data["score"]
+
+#train model
+
+model= LinearRegression()
+model.fit(X,y)
+
+#prediction
+
+a= int(input("enter the study hours:"))
+b= int(input("enter the sleep hours: "))
+c= int(input("attendance:"))
+new_data=pd.DataFrame([[a,b,c]], columns=["hours_studied", "sleep_hours", "attendance"])
+
+predition= model.predict(new_data)
+
+print("Prediction",predition[0])
